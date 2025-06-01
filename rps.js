47dcaade37929a5e7d6d5c2c3aae64aba71777f6
Camille <Camille>
2025-06-01 18:19:35 +0200

    feat: button to play new game

--- rps.js
+++ rps.js
@@ -8,21 +8,6 @@ function getComputerChoice()
   return Math.floor(Math.random() * 3);
 }
 
-function roundWinner(human_choice, computer_choice)
-{
-  let res = "";
-  if(human_choice === (computer_choice + 1) % 3) {
-    res = ". You won the round!";
-    human_score++;
-  } else if (computer_choice === (human_choice + 1) % 3) {
-    res = ". You lost the round!";
-    computer_score++;
-  } else {
-    res = ". It's a tie!";
-  }
-  return res;
-}
-
 function displayRound()
 {
   let res = document.createElement("p");
@@ -45,6 +30,51 @@ function disableButtons()
   }
 }
 
+function enableButtons()
+{
+  var allBtns = document.getElementsByTagName("button");
+  for (btn of allBtns) {
+    btn.disabled = false;
+  }
+}
+
+function reset()
+{
+  human_score = 0;
+  computer_score = 0;
+  round = 0;
+  enableButtons();
+  document.getElementById("winner").innerHTML = "";
+  document.getElementById("history").innerHTML = "";
+}
+
+function roundWinner(human_choice, computer_choice)
+{
+  let res = "";
+  if(human_choice === (computer_choice + 1) % 3) {
+    res = ". You won the round!";
+    human_score++;
+  } else if (computer_choice === (human_choice + 1) % 3) {
+    res = ". You lost the round!";
+    computer_score++;
+  } else {
+    res = ". It's a tie!";
+  }
+  return res;
+}
+
+function gameWinner()
+{
+  let btn = "<button onclick='reset()'>New game</button>";
+  if (human_score == 5) {
+      disableButtons();
+      document.getElementById("winner").innerHTML = "YOU WON THE GAME :D " + btn;
+    } else if (computer_score == 5) {
+      disableButtons();
+      document.getElementById("winner").innerHTML = "YOU LOST THE GAME :( " + btn;
+    }
+}
+
 function playRound(human_choice) {
   round++;
   let history = document.getElementById("history");
@@ -59,11 +89,5 @@ function playRound(human_choice) {
   played_round.appendChild(displayScores());
   history.prepend(played_round);
 
-  if (human_score == 5) {
-    document.getElementById("winner").textContent = "YOU WON THE GAME :D";
-    disableButtons();
-  } else if (computer_score == 5) {
-    document.getElementById("winner").textContent = "YOU LOST THE GAME :(";
-    disableButtons();
-  }
+  gameWinner();
 }
