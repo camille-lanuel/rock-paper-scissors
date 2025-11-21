85e5df84e401f3d9f20f6a8ffb037ea17be91c99
Camille <Camille>
2025-11-21 18:43:19 +0100

    removed round numbers

--- src/rps.js
+++ src/rps.js
@@ -1,19 +1,11 @@
 const CHOICES = ["Rock", "Paper", "Scissors"];
 let human_score = 0;
 let computer_score = 0;
-let round = 0;
 let human_wins = null;
 
 function getComputerChoice()
 {
   return Math.floor(Math.random() * 3);
-}
-
-function displayRound()
-{
-  let res = document.createElement("h3");
-  res.textContent = "Round " + round;
-  return res
 }
 
 function displayScores()
@@ -50,14 +42,14 @@ function reset()
   document.getElementById("round").innerHTML = "Click on a button to start the game!";
 }
 
-function roundWinner(human_choice, computer_choice)
+function roundDisplay(human_choice, computer_choice)
 {
   let res = "";
   if(human_choice === (computer_choice + 1) % 3) {
     res = "You WON the round!";
     human_score++;
   } else if (computer_choice === (human_choice + 1) % 3) {
-    res = "You LOST the round!";
+    res = "You LOST the round...";
     computer_score++;
   } else {
     res = "It's a TIE!";
@@ -70,7 +62,7 @@ function endGame()
   disableButtons();
   console.log(human_wins);
   if (human_wins) {
-    document.getElementById("endgame").innerHTML = "<p>YOU WON THE GAME :D</p>"
+    document.getElementById("endgame").innerHTML = "<p>YOU WON THE GAME :)</p>"
   } else {
     document.getElementById("endgame").innerHTML = "<p>YOU LOST THE GAME :(</p>"
   }
@@ -78,17 +70,15 @@ function endGame()
 }
 
 function playRound(human_choice) {
-  round++;
   let played_round = document.getElementById("round");
   played_round.innerHTML = "";
-  played_round.appendChild(displayRound());
 
   let computer_choice = getComputerChoice();
   let p = document.createElement("p");
   p.textContent = CHOICES[human_choice] + " VS " + CHOICES[computer_choice];
   played_round.appendChild(p);
   p = document.createElement("p");
-  p.textContent = roundWinner(human_choice, computer_choice);
+  p.textContent = roundDisplay(human_choice, computer_choice);
   played_round.appendChild(p);
   played_round.appendChild(displayScores());
 
